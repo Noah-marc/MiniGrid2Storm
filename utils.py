e62bc0489dc20bc072936b4fdc1caa9e318b815a
Noah <noah@aerocount.nl>
2025-11-29 13:59:23 +0100

    custom policy iteration for returning the scheduler as well

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 from minigrid.core.actions import Actions
 from typing import List, Dict
+from time import sleep
+from stormvogel.extensions.visual_algos import arg_max 
+
 import yaml
 import importlib
+import stormvogel
 
 
 
@@ -27,3 +31,51 @@ def process_config(env_config: Dict) -> Dict:
             }
 
     return {'env_name': env_config['name'], 'env_class': env_class,'env_params': env_config.get('class_params', {}), 'used_actions': used_actions, 'prob_distribution': prob_distribution}
+
+
+def custom_policy_iteration(model: stormvogel.model.Model,
+    prop: str,
+    visualize: bool = True,
+    layout: stormvogel.layout.Layout = stormvogel.layout.DEFAULT(),
+    delay: int = 2,
+    clear: bool = True,): 
+    """ Basically the same as policy_iteration from visual_algos.py, but returns the final scheduler as well. """
+    
+    old = None
+    new = stormvogel.random_scheduler(model)
+
+    while not old == new:
+        old = new
+
+        dtmc = old.generate_induced_dtmc()
+        dtmc_result = stormvogel.model_checking(dtmc, prop=prop)  # type: ignore
+
+        if visualize:
+            vis = stormvogel.visualization.JSVisualization(
+                model, layout=layout, scheduler=old, result=dtmc_result
+            )
+            vis.show()
+            sleep(delay)
+            if clear:
+                vis.clear()
+
+        choices = {
+            i: arg_max(
+                [
+                    lambda a: sum(
+                        [
+                            (p * dtmc_result.get_result_of_state(s2.id))  # type: ignore
+                            for p, s2 in s1.get_outgoing_choice(a)  # type: ignore
+                        ]
+                    )
+                    for _ in s1.available_actions()
+                ],
+                s1.available_actions(),
+            )
+            for i, s1 in model
+        }
+        new = stormvogel.Scheduler(model, choices)
+    if visualize:
+        print("Value iteration done:")
+        stormvogel.show(model, layout=layout, scheduler=new, result=dtmc_result)  # type: ignore
+    return dtmc_result, new  # type: ignore
