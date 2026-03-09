30d7564fdbbe7076e9d1855c942e4b58c8e55679
Noah <noah@aerocount.nl>
2026-03-09 17:10:44 +0100

    Generate final plots for last results

--- experiments/extra_plotting.py
+++ experiments/extra_plotting.py
@@ -295,8 +295,8 @@ def plot_training_comparison_subplots(dir_name: str) -> None:
 
 def main():
     # In case that this script is run directly, generate the training comparison plot for a directory.
-    plot_training_comparison("09_March_15:01")
-    plot_training_comparison_subplots("09_March_15:01")
+    plot_training_comparison("09_March_16:06")
+    plot_training_comparison_subplots("09_March_16:06")
 
 if __name__ == "__main__":
     main()
